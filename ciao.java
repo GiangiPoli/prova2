f9f3e830adedc500b11ce3894dd2b9f5c0f3b04b
Nicolas Colinucci <nicolascolinucci@MacBook-Air-di-Nicolas.local>
2026-03-16 15:42:02 +0100

    bo

--- ciao.java
+++ ciao.java
@@ -2,6 +2,8 @@ public final class ciao {
 
     public static void main(final String[] args) {
         System.out.println("ciao");
-        
+        if (true) {
+            System.out.println("no");
         }
+        
      }
