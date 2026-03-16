0fce03fd5f852ffdbd1be7552f855cdc3ebef7a7
Gianluca Poli <gianluca.poli8@studio.unibo.it>
2026-03-16 15:44:53 +0100

    aggiunta if

--- prog.java
+++ prog.java
@@ -1,6 +1,14 @@
 public class prog{
     public static void main(String[] args) {
         System.out.println("ciao");
+
+        int i = 0;
+
+        if (i == 0) {
+            System.out.println("gay");
+        } else {
+            System.out.println("frocio");
+        }
     }
 
 }
